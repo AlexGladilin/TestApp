1524bcb7d325695cc8f85ea6dba23b0815fc2d66
alexey.gladilin <alexey.gladilin@progforce.com>
2016-08-29 17:29:20 +0300

    sql

---
+++ Query.sql
@@ -0,0 +1,11 @@
+﻿select sl.ProductId, (select count(CustomerId) 
+						from (select CustomerId, (select top 1 ProductId from [Sales]
+													where CustomerId = s.CustomerId
+													order by DateCreated
+												) as FirstProduct 
+								from [Sales] s
+								group by CustomerId
+							) CustomerFirstProduct
+						where CustomerFirstProduct.FirstProduct = sl.ProductId
+					) FirstСustomerPurchase from [Sales] sl
+group by sl.ProductId
